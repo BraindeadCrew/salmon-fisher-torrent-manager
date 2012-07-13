37331ec8816ed47c819bd28c1cd0f9ba00a9f6c3
thols <thols@braindead.fr>
2012-07-13 02:44:49 +0200

    Graphical improvment, displaying finished torrents and now supporting tags

--- app.js
+++ app.js
@@ -132,57 +132,115 @@ var torrent_manager = function () {
             res.render('main.jade', {
                 title: 'Main',
                 username: '',
-		downloading:
-		[
-			{
-			name : 'qsdqsd',
-			url : 'sdfsdfsdfsff',
-			percentage : '85',
-			ratio : '1',
-			size : '452452',
-			contains : []
-			},
-			{
-			name : 'qsdq4sd',
-			url : 'sdfsdfsdfsff',
-			percentage : '46',
-			ratio : '1',
-			size : '452452',
-			contains : []
-			},
-			{
-			name : 'qsdq454sd',
-			url : 'sdfsdfsdfsff',
-			percentage : '7',
-			ratio : '1',
-			size : '452452',
-			contains : []
-			},
-			{
-			name : 'qs454545dqsd',
-			url : 'sdfsdfsdfsff',
-			percentage : '74',
-			ratio : '1',
-			size : '452452',
-			contains : []
-			},
-			{
-			name : 'qsd245245qsd',
-			url : 'sdfsdfsdfsff',
-			percentage : '45',
-			ratio : '1',
-			size : '452452',
-			contains : []
-			},
-			{
-			name : 'qsd785785qsd',
-			url : 'sdfsdfsdfsff',
-			percentage : '1',
-			ratio : '1',
-			size : '452452',
-			contains : []
-			}
-		]
+			downloading:
+			[
+				{
+				name : 'bootstrap',
+				tags : ['Developpement'],
+				percentage : '85',
+				active : true,
+				ratio : '1',
+				size : '4552',
+				contains : []
+				},
+				{
+				name : 'Die Antwoord - Tens$ion',
+				tags : ['Musique'],
+				percentage : '46',
+				active : true,
+				ratio : '1',
+				size : '452452',
+				contains : []
+				},
+				{
+				name : 'Filmographie Jean Paul Rouve',
+				tags : ['Films','Video'],
+				percentage : '7',
+				active : false,
+				ratio : '1',
+				size : '452452',
+				contains : []
+				},
+				{
+				name : 'qs454545dqsd',
+				tags : ['Serie','Video'],
+				percentage : '74',
+				active : true,
+				ratio : '1',
+				size : '452452',
+				contains : []
+				},
+				{
+				name : 'qsd245245qsd',
+				tags : ['test','sfsdf'],
+				percentage : '45',
+				active : false,
+				ratio : '1',
+				size : '452452',
+				contains : []
+				},
+				{
+				name : 'qsd785785qsd',
+				tags : ['test'],
+				percentage : '1',
+				active : false,
+				ratio : '1',
+				size : '452452',
+				contains : []
+				}
+			],
+			finished:
+			[
+				{
+				name : 'bootstrap',
+				url : '',
+				tags : ['Developpement'],
+				ratio : '0.12',
+				size : '4552',
+				contains : []
+				},
+				{
+				name : 'Die Antwoord - Tens$ion',
+				url : '',
+				tags : ['Musique'],
+				ratio : '0.62',
+				size : '452452',
+				contains : []
+				},
+				{
+				name : 'Filmographie Jean Paul Rouve',
+				url : 'sdfsdfsdfsff',
+				tags : ['Films','Video'],
+				ratio : '0.86',
+				size : '452452',
+				contains : []
+				},
+				{
+				name : 'qs454545dqsd',
+				url : 'sdfsdfsdfsff',
+				tags : ['Serie','Video'],
+				ratio : '2.01',
+				size : '452452',
+				contains : []
+				},
+				{
+				name : 'qsd245245qsd',
+				url : 'sdfsdfsdfsff',
+				tags : ['test','sfsdf'],
+				ratio : '1.62',
+				size : '452452',
+				contains : []
+				},
+				{
+				name : 'qsd785785qsd',
+				url : 'sdfsdfsdfsff',
+				tags : ['test'],
+				ratio : '0.95',
+				size : '452452',
+				contains : []
+				}
+			]
+
             });
         });
 
